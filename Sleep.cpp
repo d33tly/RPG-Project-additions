b6fba0e438d5178669afd411767b0b6578a35c4f
d33tly <wolffelliot@yahoo.com>
2016-10-26 11:36:46 -0400

    Add files via upload

---
+++ Sleep.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+/*
+Type: void
+Variables: N/A
+Purpose: Act as credits read slowly.
+*/
+void credits();
+
+int main()
+{
+credits();
+}
+
+void credits()
+{
+	cout << "++Quest for Midland++\n\n";
+	Sleep(1000);
+	cout << "C";
+	Sleep(150);
+	cout << "r";
+	Sleep(150);
+	cout << "e";
+	Sleep(150);
+	cout << "a";
+	Sleep(150);
+	cout << "t";
+	Sleep(150);
+	cout << "e";
+	Sleep(150);
+	cout << "d";
+	Sleep(150);
+	cout << " b";
+	Sleep(150);
+	cout << "y:\n";
+	Sleep(750);
+	cout << endl;
+	Sleep(750);
+	cout << "D";
+	Sleep(200);
+	cout << "a";
+	Sleep(200);
+	cout << "l";
+	Sleep(200);
+	cout << "t";
+	Sleep(200);
+	cout << "o";
+	Sleep(200);
+	cout << "n";
+	Sleep(200);
+	cout << " M";
+	Sleep(200);
+	cout << "c";
+	Sleep(200);
+	cout << "C";
+	Sleep(200);
+	cout << "l";
+	Sleep(200);
+	cout << "a";
+	Sleep(200);
+	cout << "i";
+	Sleep(200);
+	cout << "n\n";
+	Sleep(1000);
+	cout << endl;
+	Sleep(1000);
+	cout << "N";
+	Sleep(200);
+	cout << "i";
+	Sleep(200);
+	cout << "c";
+	Sleep(200);
+	cout << "a";
+	Sleep(200);
+	cout << "o";
+	Sleep(200);
+	cout << "r";
+	Sleep(200);
+	cout << " L";
+	Sleep(200);
+	cout << "a";
+	Sleep(200);
+	cout << "p";
+	Sleep(200);
+	cout << "a";
+	Sleep(200);
+	cout << "i";
+	Sleep(200);
+	cout << "x\n";
+	Sleep(1000);
+	cout << endl;
+	Sleep(1000);
+	cout << "E";
+	Sleep(200);
+	cout << "l";
+	Sleep(200);
+	cout << "l";
+	Sleep(200);
+	cout << "i";
+	Sleep(200);
+	cout << "o";
+	Sleep(200);
+	cout << "t";
+	Sleep(200);
+	cout << " W";
+	Sleep(200);
+	cout << "o";
+	Sleep(200);
+	cout << "l";
+	Sleep(200);
+	cout << "f";
+	Sleep(200);
+	cout << "f\n\n";
+	Sleep(1000);
+}
